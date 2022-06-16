44a8f42f7f3bdfe2e440acc94ba1f5ae47cc7925
kjohnbaba <105748315+kjohnbaba@users.noreply.github.com>
2022-06-16 04:48:24 -0400

    Create scanexercise.c

---
+++ scanexercise.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello User, please enter a number:");
+    
+    int a;
+    scanf("%d",&a);
+    
+    printf("Your IQ is %d",a);
+
+    return 0;
+}
